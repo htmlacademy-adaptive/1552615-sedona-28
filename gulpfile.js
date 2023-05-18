b306524c1f409d1fccb7608062d79f266b91ecac
KIlnur <kilnur1988@rambler.ru>
2023-05-18 23:25:13 +0500

    Правки после линтера

--- gulpfile.js
+++ gulpfile.js
@@ -42,7 +42,7 @@ const scripts = () => {
   return gulp.src('source/js/*.js')
     .pipe(terser())
     .pipe(gulp.dest('build/js'))
-    .pipe(browser.stream()); 
+    .pipe(browser.stream());
 }
 
 // Images
